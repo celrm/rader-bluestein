a14b624d7127dbcf4c42369910d1acdd2cb8bb05
maria-arranz <mariaarranzl@gmail.com>
2021-12-14 09:43:49 +0100

    f

--- bluestein.py
+++ bluestein.py
@@ -1,20 +1,32 @@
+import math
+from convolution import conv
+import random
 
-def bluestein(a,xi):
-    #Calculamos u(n) y v(n)
+def bluestein(a):
     n = len(a)
-    u = [a[i] * pow(xi, pow(i,2)/2) for i in n]
-    v = [pow(-xi, pow(i,2)/2) for i in n]
-    v_estrella = [pow(xi, pow(i,2)/2) for i in n]
+    xi = math.cos(math.tau / n) + 1j * math.sin(math.tau / n)
+    #Calculamos u(n) y v(n)
+    
+    u = [a[i] * pow(xi, pow(i,2)/2) for i in range(n)]
+    v = [pow(xi, -pow(i,2)/2) for i in range(n)]
+    v_estrella = [pow(xi, pow(i,2)/2) for i in range(n)]
 
-    #DFT u
-    d_u = fft(u,xi)
-    #DFT v
-    d_v = fft(v,xi)
+    u_conv_v = conv(u, v)
 
-    uv = [i*j for i,j in zip(d_u,d_v)]
-
-    i_uv = ifft(uv,xi)
-
-    dft = [i*j for i,j in zip(v_estrella,i_uv)]
+    dft = [i*j for i,j in zip(v_estrella,u_conv_v)]
 
     return dft
+
+
+# Comparar con la versión O(n²)
+def check_dft(a, dft):
+    dft_correcta = [
+        sum([a[i] * pow(math.e, math.tau * 1j * j * i / len(a)) for i in range(len(a))])
+        for j in range(len(a))
+    ]
+    print(sum([abs(dft[i] - dft_correcta[i]) for i in range(len(a))]))
+
+
+p = 11
+a = [random.uniform(0, 1) for _ in range(p)]
+check_dft(a, bluestein(a))
