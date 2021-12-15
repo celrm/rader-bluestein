4a0fe0e758386c92ad45fbd6beecbe78a0d44a5b
María Arranz <mariaarranzl@gmail.com>
2021-12-15 13:27:33 +0100

    done

--- bluestein.py
+++ bluestein.py
@@ -1,19 +1,44 @@
 import math
-from convolution import conv
 import random
 
+from fft_pot import fft,ifft
+
 def bluestein(a):
+    #Calculos basicos
     n = len(a)
-    xi = math.cos(math.tau / n) + 1j * math.sin(math.tau / n)
+    xi = pow(math.e,1j * math.tau/n)
+
     #Calculamos u(n) y v(n)
+    u = [a[i] * pow(xi,-(i*i)/2) for i in range (n)]
+    v = [pow(xi,(i *i)/2) for i in range (n)]
+    v_estrella = [pow(xi,-(i * i)/2) for i in range (n)]
+
+    #Extension a potencia de 2
+    l = 2 ** math.ceil(math.log2(2 * n + 1))
+    xi_l = math.cos(math.tau / l) + 1j * math.sin(math.tau / l)
+
+    #Extension de u
+    u_l = u + [0] * (l - n)
     
-    u = [a[i] * pow(xi, pow(i,2)/2) for i in range(n)]
-    v = [pow(xi, -pow(i,2)/2) for i in range(n)]
-    v_estrella = [pow(xi, pow(i,2)/2) for i in range(n)]
 
-    u_conv_v = conv(u, v)
+    #Extension de v
+    aux = v[1:]
+    aux.reverse()
+    v_l = v + [0] * (l - 2*n + 1) + aux
 
-    dft = [i*j for i,j in zip(v_estrella,u_conv_v)]
+    #Calculo de la convolucion
+
+    dft_u_l = fft(u_l, xi_l)  
+    dft_v_l = fft(v_l, xi_l)  
+
+    uv_l = [i*j for i,j in zip(dft_u_l,dft_v_l)]
+
+    ift_l = ifft(uv_l , xi_l) 
+
+    ift = ift_l[:n]
+
+    # multiplicamos por el factor v*
+    dft = [i*j for i,j in zip(v_estrella,ift)]
 
     return dft
 
@@ -21,7 +46,7 @@ def bluestein(a):
 # Comparar con la versión O(n²)
 def check_dft(a, dft):
     dft_correcta = [
-        sum([a[i] * pow(math.e, math.tau * 1j * j * i / len(a)) for i in range(len(a))])
+        sum([a[i] * pow(math.e, -math.tau * 1j * j * i / len(a)) for i in range(len(a))])
         for j in range(len(a))
     ]
     print(sum([abs(dft[i] - dft_correcta[i]) for i in range(len(a))]))
